7b4a6dd52d08961283e858d937ac0a96abca5b35
K.Kiyokawa <koichi20110068@gmail.com>
2021-01-07 22:27:07 +0900

    feat: use preload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,16 @@
+<script context="module" lang="ts">
+  export async function preload(page: { query: string }) {
+    return { query: page.query }
+  }
+</script>
+
+<script lang="ts">
+  export let query: string
+</script>
+
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Sapper project template. query: {query}</title>
 </svelte:head>
 
 <h1>Great success!</h1>
+<p>{query}</p>
