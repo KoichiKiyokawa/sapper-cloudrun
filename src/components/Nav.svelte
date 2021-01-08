1558aff2246e331f79d11942a363aac796ccac44
K.Kiyokawa <koichi20110068@gmail.com>
2021-01-08 17:44:04 +0900

    fix: header clickable range

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -6,25 +6,27 @@
   <h1 class="text-2xl mx-8">
     <a href="/">Sapper CloudRun</a>
   </h1>
-  <ul class="flex items-center h-full">
-    <li aria-current={segment === undefined ? 'page' : undefined}>
-      <a href=".">home</a>
+  <ul class="flex items-center">
+    <li>
+      <a aria-current={segment === undefined ? 'page' : undefined} href=".">home</a>
     </li>
-    <li aria-current={segment === 'about' ? 'page' : undefined}>
-      <a href="about">about</a>
+    <li>
+      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">about</a>
     </li>
 
     <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <li aria-current={segment === 'blog' ? 'page' : undefined}>
-      <a rel="prefetch" href="blog">blog</a>
+    <li>
+      <a aria-current={segment === 'blog' ? 'page' : undefined} rel="prefetch" href="blog">blog</a>
     </li>
   </ul>
 </nav>
 
 <style lang="sass">
-li
-  @apply h-full p-4 cursor-pointer
+li, a
+  height: 64px
+a
+  @apply flex items-center p-4
   &[aria-current]
     @apply bg-red-300
 </style>
